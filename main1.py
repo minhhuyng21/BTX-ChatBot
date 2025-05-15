3d9f1664213ea0ed67458297c25763e27a554827
Minh Huy Nguyen <minhhuy210108@gmail.com>
2025-05-15 07:32:43 +0700

    update

---
+++ main1.py
@@ -0,0 +1,142 @@
+import os
+import streamlit as st
+from langchain_community.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_openai import ChatOpenAI ,OpenAIEmbeddings
+from langchain_community.utilities import SQLDatabase
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+import time
+import speech_recognition as sr
+from dotenv import load_dotenv
+
+load_dotenv()
+SQL_KEYWORDS = [
+    "thời khóa biểu","tkb", "giáo viên", "giáo viên chủ nhiệm",
+    "lớp", "tiết", "phòng", "tiết học", "môn","thứ","cô","thầy","học sinh"
+]
+OPENAI_API_KEY = os.getenv('OPENAI')
+
+def load_model():
+    os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+    llm = ChatOpenAI(model="gpt-4o-2024-11-20")
+    embedding_model = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+    template = """<|im_start|>system
+    Sử dụng thông tin sau đây để trả lời câu hỏi. Nếu bạn không biết câu trả lời, hãy nói: "xin lỗi vì có thể dữ liệu này chưa được cập nhật".
+    {context}<|im_end|>
+    <|im_start|>user
+    {question}<|im_end|>
+    <|im_start|>assistant"""
+
+    prompt = PromptTemplate(template=template, input_variables=["context", "question"])
+    db = FAISS.load_local("faiss", embedding_model, allow_dangerous_deserialization=True)
+    llm_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever = db.as_retriever(search_kwargs={"k": 3}, max_tokens_limit=1024),
+        return_source_documents=False,
+        chain_type_kwargs={'prompt': prompt}
+    )
+    # Gọi mô hình LLM để lấy phản hồi
+    sql = SQLDatabase.from_uri("sqlite:///tkb_sql.db")
+    toolkit = SQLDatabaseToolkit(db=sql, llm=llm)
+    agent_executor = create_sql_agent(llm, db=sql, agent_type="openai-tools", verbose=True)
+    return llm_chain, agent_executor
+
+
+def is_sql_query(user_input: str) -> bool:
+    text = user_input.lower()
+    return any(kw in text for kw in SQL_KEYWORDS)
+
+def response_generator(user_input):
+    # 1. Kiểm tra intent
+    if is_sql_query(user_input):
+        # 2a. Dùng SQL agent
+        try:
+            answer = st.session_state['sql'].invoke(user_input.upper())['output']
+        except:
+            answer = "Có lỗi truy vấn dữ liệu, vui lòng thử lại."
+    else:
+        # 2b. Dùng LLM
+        print('in')
+        try:
+            response = st.session_state['model'].invoke({"query": user_input + " và trả lời bằng tiếng việt"})
+            answer = response['result']
+        except:
+            answer = "Có lỗi xử lý ngôn ngữ, vui lòng thử lại."
+
+    # 3. Stream từng từ
+    for w in answer.split():
+        yield w + " "
+        time.sleep(0.05)
+
+def recognize_speech():
+    # Tạo container chính để chứa toàn bộ nội dung
+    main_container = st.empty()
+    
+    with main_container.container():
+        recognizer = sr.Recognizer()
+        with sr.Microphone() as source:
+            st.info("Đang thu âm... Hãy nói vào microphone.")
+            audio = recognizer.listen(source)
+        
+        # Tạo container cho thông báo trạng thái
+        status_container = st.empty()
+        
+        try:
+            # Nhận dạng giọng nói
+            text = recognizer.recognize_google(audio, language="vi-VN")
+            status_container.success("Nhận dạng thành công!")
+            time.sleep(5)  # Đợi 5 giây
+            
+            # Xóa toàn bộ nội dung
+            main_container.empty()
+            
+            return text
+            
+        except sr.UnknownValueError:
+            status_container.error("Không thể nhận dạng giọng nói.")
+        except sr.RequestError:
+            status_container.error("Lỗi kết nối với dịch vụ nhận dạng.")
+        
+        # Xóa toàn bộ nội dung sau 5 giây nếu có lỗi
+        time.sleep(5)
+        main_container.empty()
+        return ""
+
+def main():
+    st.title("Trợ lí ảo Bùi Thị Xuân")
+    voice_button = st.button("VOICE")
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    user_input = st.chat_input("Nhập promt")
+    if voice_button:
+        user_input = recognize_speech()
+    if user_input:      
+        # Lưu cuộc hội thoại người dùng
+        st.session_state['messages'].append({"role": "user", "content": user_input})
+
+        # Hiển thị cuộc hội thoại người dùng
+        with st.chat_message("user"):
+            st.markdown(user_input)
+
+        # Hiển thị cuộc hội thoại AI
+        with st.chat_message("assistant"):
+            response = st.write_stream(response_generator(user_input))
+
+        # Lưu cuộc hội thoại AI  
+        st.session_state.messages.append({"role": "assistant", "content": response})    
+
+if __name__ == '__main__':
+    if 'model' not in st.session_state and 'sql' not in st.session_state:
+        st.session_state['model'], st.session_state['sql'] = load_model()
+
+
+    main()
